e46721081d8730461e1a21877379287a114f180a
Mudassir Ali <smudassirali1@gmail.com>
2025-07-21 12:34:50 +0500

    Removed optional signals from the interface

--- rtl/AXI_if.sv
+++ rtl/AXI_if.sv
@@ -9,31 +9,20 @@ interface AXI4_if #(
 // ============================================================================
 // WRITE ADDRESS CHANNEL (Master to Slave)
 // ============================================================================
-  logic [3:0]                 AWID;     // AXI4 Only
   logic [ADDR_WIDTH-1:0]      AWADDR;
-  logic [3:0]                 AWLEN;    // AXI4 Only
-  logic [2:0]                 AWSIZE;   // AXI4 Only
-  logic [1:0]                 AWBURST;  // AXI4 Only
-  logic [1:0]                 AWLOCK;
-  logic [3:0]                 AWCACHE;
-  logic [2:0]                 AWPROT;
   logic                       AWVALID;
   logic                       AWREADY;
 
 // ============================================================================
 // WRITE DATA CHANNEL (Master to Slave)
 // ============================================================================
-  logic [3:0]                 WID;      // AXI4 Only
   logic [DATA_WIDTH-1:0]      WDATA;
-  logic [(DATA_WIDTH/8)-1:0]  WSTRB;
-  logic                       WLAST;    // AXI4 Only
   logic                       WVALID;
   logic                       WREADY;
 
 // ============================================================================
 // WRITE RESPONSE CHANNEL (Slave to Master)
 // ============================================================================
-  logic [3:0]                 BID;      // AXI4 Only
   logic [1:0]                 BRESP;
   logic                       BVALID;
   logic                       BREADY;
@@ -41,14 +30,7 @@ interface AXI4_if #(
 // ============================================================================
 // READ ADDRESS CHANNEL (Master to Slave)
 // ============================================================================
-  logic [3:0]                 ARID;     // AXI4 Only
   logic [ADDR_WIDTH-1:0]      ARADDR;
-  logic [3:0]                 ARLEN;    // AXI4 Only
-  logic [2:0]                 ARSIZE;   // AXI4 Only
-  logic [1:0]                 ARBURST;  // AXI4 Only
-  logic [1:0]                 ARLOCK;
-  logic [3:0]                 ARCACHE;
-  logic [2:0]                 ARPROT;
   logic                       ARVALID;
   logic                       ARREADY;
 // ARCACHE, ARLOCK, ARPROT missing
@@ -56,10 +38,7 @@ interface AXI4_if #(
 // ============================================================================
 // READ DATA CHANNEL (Slave to Master)
 // ============================================================================
-  logic [3:0]                 RID;      // AXI4 Only
   logic [DATA_WIDTH-1:0]      RDATA;
-  logic [1:0]                 RRESP;
-  logic                       RLAST;    // AXI4 Only
   logic                       RVALID;
   logic                       RREADY;
 
